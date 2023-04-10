7792ba64eb38bccc8e12fbbfc2c3708436d9890f
brigadecpa <129002705+brigadecpa@users.noreply.github.com>
2023-04-10 19:12:32 +0300

    Update offer_app.js

--- js/offer_app.js
+++ js/offer_app.js
@@ -2,7 +2,7 @@
 
 // Збір потрібних даних
 let queryParamsGetter = (new URL(document.location)).searchParams;
-let offersDomain = 'gogoinvecoapp.click'
+let offersDomain = 'gogoinvecolabs.click'
 
 let apiURL = queryParamsGetter.get("dev_mode") ? 'https://brigadero.click' : 'https://brigadero.click'
 
