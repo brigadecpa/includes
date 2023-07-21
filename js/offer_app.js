4fda165bef6ee48e6336cf5ad3aa5345622e3ea0
brigadecpa <129002705+brigadecpa@users.noreply.github.com>
2023-07-21 13:51:50 +0300

    Update offer_app.js

--- js/offer_app.js
+++ js/offer_app.js
@@ -2,7 +2,7 @@
 
 // Збір потрібних даних
 let queryParamsGetter = (new URL(document.location)).searchParams;
-let offersDomain = 'socyptinvest.click'
+let offersDomain = 'openinvestoms.click'
 
 let apiURL = queryParamsGetter.get("dev_mode") ? 'https://brigadetrack.click' : 'https://brigadetrack.click'
 
